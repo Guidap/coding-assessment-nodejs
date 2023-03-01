c2751a5bdb9516b9337b194bd533ae8d93f05848
Baptiste Raymond <11996931+belisarus@users.noreply.github.com>
2023-03-01 12:47:56 +0100

    update log and node version

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,5 +5,4 @@ app.get('/', function(req, res) {
   res.json({message: 'hello world API'});
 });
 
-app.listen(8000)
-console.log('Running on http://localhost:8000');
+app.listen(8000, () => console.log('Running on http://localhost:8000'))
